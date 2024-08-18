7240d256d79432c9d551ee4df084d3b7432af060
IT21314742 <it21314742@my.sliit.lk>
2024-08-18 11:13:42 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ while True:
     ret, frame = cap.read()
 
     image = np.zeros( frame.shape, np.uint8)
-    smaller
+    smaller_
     cv2.imshow('frame', image)
 
     if cv2.waitKey(1) == ord('q'):
