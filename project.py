9cdc7534cae45aa8339dd75a93a15405c0fc41bf
IT21314742 <it21314742@my.sliit.lk>
2024-08-18 12:10:14 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -1,3 +1,4 @@
+import cv2.cv2
 import numpy as np
 import cv2
 
@@ -10,9 +11,9 @@ while True:
 
     image = np.zeros( frame.shape, np.uint8)
     smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-    image[:height//2, :width//2] = cv2.rotate(smaller_frame)
-    image[height//2:, :width//2] = smaller_frame
-    image[:height//2, width//2:] = smaller_frame
+    image[:height//2, :width//2] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
+    image[height//2:, :width//2] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_90_CLOCKWISE)
+    image[:height//2, width//2:] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_180)
     image[height//2:, width//2:] = smaller_frame
 
     cv2.imshow('frame', image)
