612f36b2cdfb5c7ca45cdffdfdfd068557523324
IT21314742 <it21314742@my.sliit.lk>
2024-08-18 12:12:02 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -1,6 +1,5 @@
-import cv2.cv2
-import numpy as np
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 
@@ -9,11 +8,11 @@ while True:
     width = int(cap.get(3))
     height = int(cap.get(4))
 
-    image = np.zeros( frame.shape, np.uint8)
+    image = np.zeros(frame.shape, np.uint8)
     smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-    image[:height//2, :width//2] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
-    image[height//2:, :width//2] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_90_CLOCKWISE)
-    image[:height//2, width//2:] = cv2.rotate(smaller_frame, cv2.cv2.ROTATE_180)
+    image[:height//2, :width//2] = cv2.rotate(smaller_frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    image[height//2:, :width//2] = cv2.rotate(smaller_frame, cv2.ROTATE_90_CLOCKWISE)
+    image[:height//2, width//2:] = cv2.rotate(smaller_frame, cv2.ROTATE_180)
     image[height//2:, width//2:] = smaller_frame
 
     cv2.imshow('frame', image)
