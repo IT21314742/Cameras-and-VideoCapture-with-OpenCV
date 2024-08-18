052d55cb65144ac0487e2dfb9024d028a6285fc5
IT21314742 <it21314742@my.sliit.lk>
2024-08-18 11:20:07 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -10,7 +10,7 @@ while True:
 
     image = np.zeros( frame.shape, np.uint8)
     smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-    image[:height//2, ]
+    image[:height//2, :width//2] = 
     cv2.imshow('frame', image)
 
     if cv2.waitKey(1) == ord('q'):
