15b1866675fe23bd8460f6499776bbbee825b4a8
IT21314742 <it21314742@my.sliit.lk>
2024-08-18 11:17:06 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -5,9 +5,11 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
+    width
 
     image = np.zeros( frame.shape, np.uint8)
     smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+
     cv2.imshow('frame', image)
 
     if cv2.waitKey(1) == ord('q'):
