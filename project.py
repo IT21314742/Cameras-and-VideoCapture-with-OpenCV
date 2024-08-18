27d243a712bede17b3a5f046eea80742fa71c0bd
IT21314742 <it21314742@my.sliit.lk>
2024-08-18 11:22:08 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -11,7 +11,7 @@ while True:
     image = np.zeros( frame.shape, np.uint8)
     smaller_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
     image[:height//2, :width//2] = smaller_frame
-    image[:height//2, :width//2] = smaller_frame
+    image[height//2:, :width//2] = smaller_frame
     image[:height//2, :width//2] = smaller_frame
     image[:height//2, :width//2] = smaller_frame
 
